19237b47a53861e37a3f464d4633d50d17db0f0b
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-27 20:20:38 -0500

    Memory.cs: Added Memory Register Variables

--- Source/GameBeak-Frontend/Classes/Memory.cs
+++ Source/GameBeak-Frontend/Classes/Memory.cs
@@ -11,6 +11,10 @@ namespace GameBeak_Frontend.Classes
     class Memory
     {
 
+        private short regAF = 0;
+        private short regBC = 0;
+        private short regDE = 0;
+        private short regHL = 0;
 
 
 
