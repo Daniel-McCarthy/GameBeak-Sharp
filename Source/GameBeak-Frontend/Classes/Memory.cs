ceb5252a7a2d7eaade040021f026e02242bc532d
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-27 20:23:23 -0500

    Memory.cs: Ported readRomHeader() to C#

--- Source/GameBeak-Frontend/Classes/Memory.cs
+++ Source/GameBeak-Frontend/Classes/Memory.cs
@@ -29,6 +29,87 @@ namespace GameBeak_Frontend.Classes
         private ushort memoryPointer = 0;
 
 
+        void readRomHeader()
+        {
+
+            for (int i = 0; i < 16; i++)
+            {
+                title += (char)rom[0x134 + i];
+            }
+
+            //0x147 Catridge type
+            /*
+            00 Rom only | 01 MBC1 | 02 MBC1 + Ram | 03 MBC1 + Ram + Battery | 05 MBC2 | 06 MBC2 + Battery | 08 Rom + Ram |
+            09 Rom + Ram + Battery | 0B MMM01 | 0C MMM01 + Ram | 0D MMM01 + Ram + Battery | 11 MBC3 | 12 MBC3 + Ram |
+            13 MBC3 + Ram + Battery | 19 MBC5 | 1A MBC5 + Ram | 1B MBC5 + Ram + Battery | 1C MBC5 + Rumble | 1D MBC5 + Rumble + Ram |
+            1E MBC5 + Rumble + Ram + Battery | 20 MBC6 | 22 MBC7 + Sensor + Rumble + Ram + Battery | FC Pocket Camera |
+            FD Bandai Tama5 | FE HuC3 | FF HuC1 + Ram + Battery |
+            */
+
+            byte cartridgeType = rom[0x147];
+            byte romSize = rom[0x148];
+            memoryControllerMode = cartridgeType;
+            if (memoryControllerMode == 0)
+            {
+                //None
+                writeFullRomToRam();
+            }
+            else if (memoryControllerMode <= 3)
+            {
+                //MBC1
+                writeRom0ToRam();
+
+                changeMBC1RomBanks(1);
+            }
+            else if (memoryControllerMode <= 6)
+            {
+                //MBC2
+                writeRom0ToRam();
+
+                changeMBC2RomBanks(1);
+            }
+            else if (memoryControllerMode <= 9)
+            {
+                //8: Rom+Ram
+                //9: Rom+Ram+Battery
+
+                writeFullRomToRam();
+            }
+            else if (memoryControllerMode <= 0x0D)
+            {
+                //0B: MMM01
+                //0C: MMM01+Ram
+                //0D: MMM01+Ram+Battery
+
+                writeRom0ToRam();
+            }
+            else if (memoryControllerMode <= 0x10)
+            {
+                //0F: MBC3+Timer+Battery
+                //10: MBC3+Timer+Ram+Battery
+                //11: MBC3
+                //12: MBC3+Ram
+                //13: MBC3+Ram+Battery
+
+                writeRom0ToRam();
+
+                changeMBC3RomBanks(1);
+            }
+            else if (memoryControllerMode <= 0x1E)
+            {
+
+                writeRom0ToRam();
+
+                changeMBC5RomBanks(1);
+            }
+            else
+            {
+
+                writeFullRomToRam();
+                //writeRom0ToRam();
+            }
+
+        }
 
 
 
