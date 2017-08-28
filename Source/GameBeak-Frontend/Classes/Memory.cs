b518bef6ae70c49a17411c086d3c024218fd5666
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-27 20:20:01 -0500

    Creation of Memory.cs Class (C++ Port to C#)
    -Created the Memory.cs Class file. This will host the port of the C++
    Memory.h class to C#.

---
+++ Source/GameBeak-Frontend/Classes/Memory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GameBeak_Frontend.Classes
+{
+    class Memory
+    {
+
+
+
+
+
+
+
+    }
+}
