d57b1750e8f555f4c65352ef00ff279e225fa019
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-03 19:02:02 -0500

    GameBeakC#: Ported Color class to C#

---
+++ Source/GameBeak-Frontend/Classes/Color.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBeak_Frontend.Classes
+{
+    class Color
+    {
+
+        public static Color Black = new Color(0, 0, 0, 255);
+        public static Color White = new Color(255, 255, 255, 255);
+
+
+        public byte r;
+        public byte g;
+        public byte b;
+        public byte a;
+
+        public Color()
+        {
+	        r = g = b = a = 0;
+        }
+
+        public Color(byte newR, byte newG, byte newB)
+        {
+	        r = newR;
+	        g = newG;
+	        b = newB;
+	        a = 255;
+        }
+
+        public Color(byte newR, byte newG, byte newB, byte newA)
+        {
+	        r = newR;
+	        g = newG;
+	        b = newB;
+	        a = newA;
+        }
+
+        public uint getInt()
+        {
+            return (uint)((r << 24) | (g << 16) | (b << 8) | a);
+        }
+
+    }
+}
