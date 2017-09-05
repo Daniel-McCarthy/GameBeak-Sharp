e7252f203dbe684d154b26cbedb6a7d35152ba5d
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-04 22:19:56 -0500

    GameBeakC#: Updated Static Core with Constructor
    -Added constructor which initializes all static variables in core class.

--- Source/GameBeak-Frontend/Classes/Core.cs
+++ Source/GameBeak-Frontend/Classes/Core.cs
@@ -8,7 +8,43 @@ namespace GameBeak_Frontend.Classes
 {
     static class Core
     {
-        public static Memory beakMemory = new Memory();
-        public static GPU beakGPU = new GPU();
+        static Core()
+        {
+            beakMemory = new Memory();
+            beakGPU = new GPU();
+            beakWindow = new GameWindow();
+            beakCPU = new CPU();
+
+            clocks = 4500;
+
+            run = true;
+            paused = false;
+            step = false;
+            enableInterruptsNextCycle = false;
+            paletteSetting = 0;
+        }
+        
+
+        public static Memory beakMemory;
+        public static GPU beakGPU;
+        public static GameWindow beakWindow;
+        public static CPU beakCPU;
+
+        public static int clocks;
+
+        //extern int memoryControllerMode;
+        public static bool run;
+        public static bool paused;
+        public static bool step;
+        public static bool enableInterruptsNextCycle;
+        public static byte paletteSetting;
+        //extern byte filterSetting;
+
+
+        //extern Audio beakAudio;
+
+        //extern KeyInput beakInput;
+    }
+
     }
 }
