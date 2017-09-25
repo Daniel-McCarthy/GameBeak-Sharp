fb3d2abdbd0a89b68b6d2787a01ea160a1ad1f74
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-25 01:42:46 -0500

    Added Input Variables to Input Class
    These tell the class what keys are currently pressed. These will be
    updated as key states change. ReadInput will check these when it is time
    for input.

--- Source/GameBeak-Frontend/Classes/Input.cs
+++ Source/GameBeak-Frontend/Classes/Input.cs
@@ -9,6 +9,22 @@ namespace GameBeak_Frontend.Classes
     class Input
     {
 
+        //Gameboy keys:
+        //[Up][Left][Right][Down][A][B][Start][Select]
+
+        //Mapped to standard keyboard keys:
+        //[Up][Left][Right][Down][Z][X][Enter][RShift]
+
+
+	    bool keyUp = false;
+        bool keyDown = false;
+        bool keyLeft = false;
+        bool keyRight = false;
+        bool keyStart = false;
+        bool keySelect = false;
+        bool keyA = false;
+        bool keyB = false;
+
 
 
     }
