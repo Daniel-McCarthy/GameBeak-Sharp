43e34d84f3e31380bd19e229006eaa020de07fb7
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-29 22:04:33 -0500

    Color: Added Overrides for == and != Operators
    Previously the color class checked if the two objects were the same
    instance. Now these operators are used to check if their values are the
    same.

--- Source/GameBeak-Frontend/Classes/Color.cs
+++ Source/GameBeak-Frontend/Classes/Color.cs
@@ -77,5 +77,16 @@ namespace GameBeak_Frontend.Classes
             return (uint)((a << 24) | (r << 16) | (g << 8) | b);
         }
 
+
+        public static bool operator == (Color color1, Color color2)
+        {
+            return (color1.r == color2.r) && (color1.g == color2.g) && (color1.b == color2.b) && (color1.a == color2.a);
+        }
+
+        public static bool operator != (Color color1, Color color2)
+        {
+            return (color1.r != color2.r) || (color1.g != color2.g) || (color1.b != color2.b) || (color1.a != color2.a);
+        }
+
     }
 }
