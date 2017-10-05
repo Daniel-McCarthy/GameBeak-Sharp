87f4296131ad5a84b7c7aeccf444291beb6cc0d3
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-05 14:41:51 -0500

    PaletteView: Added New, Delete, and Save to File Buttons

--- Source/GameBeak-Frontend/Forms/PaletteEditor.Designer.cs
+++ Source/GameBeak-Frontend/Forms/PaletteEditor.Designer.cs
@@ -50,6 +50,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.paletteSelectGroupBox.SuspendLayout();
             this.palettePreviewGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bgColorPreview1)).BeginInit();
@@ -69,17 +72,20 @@
             // paletteSelectGroupBox
             // 
             this.paletteSelectGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.paletteSelectGroupBox.Controls.Add(this.button3);
+            this.paletteSelectGroupBox.Controls.Add(this.button2);
+            this.paletteSelectGroupBox.Controls.Add(this.button1);
             this.paletteSelectGroupBox.Controls.Add(this.paletteNameListBox);
             this.paletteSelectGroupBox.Location = new System.Drawing.Point(12, 12);
             this.paletteSelectGroupBox.Name = "paletteSelectGroupBox";
-            this.paletteSelectGroupBox.Size = new System.Drawing.Size(260, 131);
+            this.paletteSelectGroupBox.Size = new System.Drawing.Size(260, 154);
             this.paletteSelectGroupBox.TabIndex = 0;
             this.paletteSelectGroupBox.TabStop = false;
             this.paletteSelectGroupBox.Text = "Palette Select";
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(197, 288);
+            this.okButton.Location = new System.Drawing.Point(197, 302);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 1;
@@ -105,9 +111,9 @@
             this.palettePreviewGroupBox.Controls.Add(this.bgColorPreview3);
             this.palettePreviewGroupBox.Controls.Add(this.bgColorPreview2);
             this.palettePreviewGroupBox.Controls.Add(this.bgColorPreview1);
-            this.palettePreviewGroupBox.Location = new System.Drawing.Point(12, 149);
+            this.palettePreviewGroupBox.Location = new System.Drawing.Point(12, 172);
             this.palettePreviewGroupBox.Name = "palettePreviewGroupBox";
-            this.palettePreviewGroupBox.Size = new System.Drawing.Size(260, 133);
+            this.palettePreviewGroupBox.Size = new System.Drawing.Size(260, 124);
             this.palettePreviewGroupBox.TabIndex = 2;
             this.palettePreviewGroupBox.TabStop = false;
             this.palettePreviewGroupBox.Text = "Palette Preview";
@@ -125,7 +131,7 @@
             // bgColorPreview1
             // 
             this.bgColorPreview1.BackColor = System.Drawing.Color.White;
-            this.bgColorPreview1.Location = new System.Drawing.Point(67, 19);
+            this.bgColorPreview1.Location = new System.Drawing.Point(67, 16);
             this.bgColorPreview1.Name = "bgColorPreview1";
             this.bgColorPreview1.Size = new System.Drawing.Size(32, 32);
             this.bgColorPreview1.TabIndex = 0;
@@ -135,7 +141,7 @@
             // bgColorPreview2
             // 
             this.bgColorPreview2.BackColor = System.Drawing.Color.White;
-            this.bgColorPreview2.Location = new System.Drawing.Point(111, 19);
+            this.bgColorPreview2.Location = new System.Drawing.Point(112, 16);
             this.bgColorPreview2.Name = "bgColorPreview2";
             this.bgColorPreview2.Size = new System.Drawing.Size(32, 32);
             this.bgColorPreview2.TabIndex = 1;
@@ -145,7 +151,7 @@
             // bgColorPreview4
             // 
             this.bgColorPreview4.BackColor = System.Drawing.Color.White;
-            this.bgColorPreview4.Location = new System.Drawing.Point(198, 19);
+            this.bgColorPreview4.Location = new System.Drawing.Point(199, 16);
             this.bgColorPreview4.Name = "bgColorPreview4";
             this.bgColorPreview4.Size = new System.Drawing.Size(32, 32);
             this.bgColorPreview4.TabIndex = 3;
@@ -155,7 +161,7 @@
             // bgColorPreview3
             // 
             this.bgColorPreview3.BackColor = System.Drawing.Color.White;
-            this.bgColorPreview3.Location = new System.Drawing.Point(156, 19);
+            this.bgColorPreview3.Location = new System.Drawing.Point(157, 16);
             this.bgColorPreview3.Name = "bgColorPreview3";
             this.bgColorPreview3.Size = new System.Drawing.Size(32, 32);
             this.bgColorPreview3.TabIndex = 2;
@@ -164,7 +170,7 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(111, 288);
+            this.cancelButton.Location = new System.Drawing.Point(111, 302);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 3;
@@ -175,7 +181,7 @@
             // bp0ColorPreview4
             // 
             this.bp0ColorPreview4.BackColor = System.Drawing.Color.White;
-            this.bp0ColorPreview4.Location = new System.Drawing.Point(199, 57);
+            this.bp0ColorPreview4.Location = new System.Drawing.Point(199, 51);
             this.bp0ColorPreview4.Name = "bp0ColorPreview4";
             this.bp0ColorPreview4.Size = new System.Drawing.Size(32, 32);
             this.bp0ColorPreview4.TabIndex = 7;
@@ -185,7 +191,7 @@
             // bp0ColorPreview3
             // 
             this.bp0ColorPreview3.BackColor = System.Drawing.Color.White;
-            this.bp0ColorPreview3.Location = new System.Drawing.Point(157, 57);
+            this.bp0ColorPreview3.Location = new System.Drawing.Point(157, 51);
             this.bp0ColorPreview3.Name = "bp0ColorPreview3";
             this.bp0ColorPreview3.Size = new System.Drawing.Size(32, 32);
             this.bp0ColorPreview3.TabIndex = 6;
@@ -195,7 +201,7 @@
             // bp0ColorPreview2
             // 
             this.bp0ColorPreview2.BackColor = System.Drawing.Color.White;
-            this.bp0ColorPreview2.Location = new System.Drawing.Point(112, 57);
+            this.bp0ColorPreview2.Location = new System.Drawing.Point(112, 51);
             this.bp0ColorPreview2.Name = "bp0ColorPreview2";
             this.bp0ColorPreview2.Size = new System.Drawing.Size(32, 32);
             this.bp0ColorPreview2.TabIndex = 5;
@@ -205,7 +211,7 @@
             // bp0ColorPreview1
             // 
             this.bp0ColorPreview1.BackColor = System.Drawing.Color.White;
-            this.bp0ColorPreview1.Location = new System.Drawing.Point(67, 57);
+            this.bp0ColorPreview1.Location = new System.Drawing.Point(67, 51);
             this.bp0ColorPreview1.Name = "bp0ColorPreview1";
             this.bp0ColorPreview1.Size = new System.Drawing.Size(32, 32);
             this.bp0ColorPreview1.TabIndex = 4;
@@ -215,7 +221,7 @@
             // bp1ColorPreview4
             // 
             this.bp1ColorPreview4.BackColor = System.Drawing.Color.White;
-            this.bp1ColorPreview4.Location = new System.Drawing.Point(199, 95);
+            this.bp1ColorPreview4.Location = new System.Drawing.Point(199, 87);
             this.bp1ColorPreview4.Name = "bp1ColorPreview4";
             this.bp1ColorPreview4.Size = new System.Drawing.Size(32, 32);
             this.bp1ColorPreview4.TabIndex = 11;
@@ -225,7 +231,7 @@
             // bp1ColorPreview3
             // 
             this.bp1ColorPreview3.BackColor = System.Drawing.Color.White;
-            this.bp1ColorPreview3.Location = new System.Drawing.Point(157, 95);
+            this.bp1ColorPreview3.Location = new System.Drawing.Point(157, 87);
             this.bp1ColorPreview3.Name = "bp1ColorPreview3";
             this.bp1ColorPreview3.Size = new System.Drawing.Size(32, 32);
             this.bp1ColorPreview3.TabIndex = 10;
@@ -235,7 +241,7 @@
             // bp1ColorPreview2
             // 
             this.bp1ColorPreview2.BackColor = System.Drawing.Color.White;
-            this.bp1ColorPreview2.Location = new System.Drawing.Point(112, 95);
+            this.bp1ColorPreview2.Location = new System.Drawing.Point(112, 87);
             this.bp1ColorPreview2.Name = "bp1ColorPreview2";
             this.bp1ColorPreview2.Size = new System.Drawing.Size(32, 32);
             this.bp1ColorPreview2.TabIndex = 9;
@@ -245,7 +251,7 @@
             // bp1ColorPreview1
             // 
             this.bp1ColorPreview1.BackColor = System.Drawing.Color.White;
-            this.bp1ColorPreview1.Location = new System.Drawing.Point(67, 95);
+            this.bp1ColorPreview1.Location = new System.Drawing.Point(67, 87);
             this.bp1ColorPreview1.Name = "bp1ColorPreview1";
             this.bp1ColorPreview1.Size = new System.Drawing.Size(32, 32);
             this.bp1ColorPreview1.TabIndex = 8;
@@ -255,7 +261,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Location = new System.Drawing.Point(16, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 13);
             this.label1.TabIndex = 12;
@@ -264,7 +270,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 57);
+            this.label2.Location = new System.Drawing.Point(16, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 13;
@@ -273,18 +279,45 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 95);
+            this.label3.Location = new System.Drawing.Point(16, 89);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(30, 13);
             this.label3.TabIndex = 14;
             this.label3.Text = "BP1:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(135, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save All to File";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // PaletteEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(221)))), ((int)(((byte)(221)))));
-            this.ClientSize = new System.Drawing.Size(284, 323);
+            this.ClientSize = new System.Drawing.Size(284, 337);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.palettePreviewGroupBox);
             this.Controls.Add(this.okButton);
@@ -336,5 +369,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
