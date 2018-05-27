3995cd6290c0b055b539b823f438abe50c39d512
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-27 16:16:57 -0500

    Added Breakpoint Address Input Box

---
+++ Source/GameBeak-Frontend/Forms/General Use/BreakpointInputBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameBeak.Forms
+{
+    public partial class BreakpointInputBox : GameBeak.Forms.InputBox
+    {
+        public BreakpointInputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void BreakpointInputBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult == DialogResult.OK)
+            {
+                string textToVerify = inputTextBox.Text;
+                int textLength = textToVerify.Length;
+
+                if (textLength == 0 || textLength > 4)
+                {
+                    e.Cancel = true;
+
+                    MessageBox.Show("Address length must be 1 to 4 hexadecimal characters.");
+                }
+
+                ushort address = 0;
+                bool parseSuccess = ushort.TryParse(textToVerify, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.CurrentCulture, out address);
+
+                if (!parseSuccess)
+                {
+                    e.Cancel = true;
+
+                    MessageBox.Show("This is not a valid hexadecimal address.");
+                }
+            }
+        }
+    }
+}
