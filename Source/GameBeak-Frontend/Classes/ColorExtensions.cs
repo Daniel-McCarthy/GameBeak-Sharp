b2bea6c501c841526729560b571c854382426a0d
Dan McCarthy <wererabbbit@yahoo.com>
2018-07-01 20:03:19 -0500

    ColorExtensions: Added Math.Max helper functions for three variables

--- Source/GameBeak-Frontend/Classes/ColorExtensions.cs
+++ Source/GameBeak-Frontend/Classes/ColorExtensions.cs
@@ -8,5 +8,14 @@ namespace GameBeak.Classes
 {
     class ColorExtensions
     {
+        private static byte maxValue(byte a, byte b, byte c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
+        private static float maxValue(float a, float b, float c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
+
     }
 }
