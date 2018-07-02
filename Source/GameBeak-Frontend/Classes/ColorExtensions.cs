cb885719215363d2a5c4d96db5df60f2d4964207
Dan McCarthy <wererabbbit@yahoo.com>
2018-07-01 20:04:01 -0500

    ColorExtensions: Added float[] rgbToHSL function

--- Source/GameBeak-Frontend/Classes/ColorExtensions.cs
+++ Source/GameBeak-Frontend/Classes/ColorExtensions.cs
@@ -26,5 +26,44 @@ namespace GameBeak.Classes
             return Math.Min(a, Math.Min(b, c));
         }
 
+        public static float[] rgbToHSL(byte rVal, byte gVal, byte bVal)
+        {
+            float r = rVal / 255f;
+            float g = gVal / 255f;
+            float b = bVal / 255f;
+            float max = maxValue(r, g, b);
+            float min = minValue(r, g, b);
+            float intensity = (r + g + b) * (1f / 3f);
+
+            // Calculate Chroma.
+            float chroma = max - min;
+
+            // Calculate Hue.
+            float hue = 0;
+
+            if (max == r)
+            {
+                hue = ((g - b) / chroma) % 6;
+            }
+            else if (max == g)
+            {
+                hue = ((b - r) / chroma) + 2;
+            }
+            else if (max == b)
+            {
+                hue = ((r - g) / chroma) + 4;
+            }
+
+            hue *= 60f;
+
+            // Calculate Saturation.
+            float saturation = (intensity == 0) ? 0 : (1 - (min / intensity));
+
+            // Calculate Lightness.
+            float lightness = (max + min) * .5f;
+
+            return new float[] { hue, saturation, lightness };
+        }
+
     }
 }
